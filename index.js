5da767827931bbebdf3f9e14b3af3bc5728a3e16
Michael Wood <michaelwood33311@icloud.com>
2023-04-15 08:33:35 -0400

    added adding point to qdrant

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const qdrant = require ('./qdrant');
 
-
-
-
 async function run () {
     //const response = await qdrant.createCollection('test1', 4);
-    const response = await qdrant.confirmCollection('test1');
+    //const response = await qdrant.confirmCollection('test1');
+    const response = await qdrant.addPoint('test1', {
+        vector: [0.05, 0.61, 0.76, 0.74], payload: {"city": "Berlin" }
+    });
     
     console.log(response.data);
 }
--- qdrant.js
+++ qdrant.js
@@ -1,5 +1,6 @@
 require ('dotenv').config();
 const axios = require('axios');
+const { v4: uuidv4 } = require('uuid');
 
 const host = process.env.QDRANT_HOST;
 const port = process.env.QDRANT_PORT;
@@ -27,6 +28,34 @@ exports.confirmCollection = async (collectionName) => {
     const request = {
         url: `${host}:${port}/collections/${collectionName}`,
         method: 'get'
+    }
+
+    return axios(request);
+}
+
+/*
+ * point: { vector, payload}
+ */
+
+exports.addPoint = async (collectionName, point) => {
+    const { vector, payload } = point;
+    const id = uuidv4();
+
+    const request = {
+        url: `${host}:${port}/collections/${collectionName}/points`,
+        method: 'put',
+        headers: {
+            'Content-Type': 'application/json;charset=UTF-8',
+            "Access-Control-Allow-Origin": "*",
+        },
+        data: {
+            points: [
+                {
+                    id, vector, payload
+
+                }
+            ]
+        }
     }
 
     return axios(request);
